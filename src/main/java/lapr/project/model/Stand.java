60a7a17beb79c6990d2a149934c1b4ab943e6a53
Ricardo António de Sousa Osório <1150696@isep.ipp.pt>
2016-06-08 19:53:51 +0100

    Adicionado método criarStand para o UC23 Corrigidos pequenos erros encontrados [Implementation][UC-23]

--- src/main/java/lapr/project/model/Stand.java
+++ src/main/java/lapr/project/model/Stand.java
@@ -5,7 +5,7 @@ package lapr.project.model;
  * @author Ricardo Catalao
  */
 public class Stand {
-    
+
     private String m_ID;
     private int m_area;
 
@@ -40,5 +40,20 @@ public class Stand {
      */
     public void setM_area(int area) {
         this.m_area = area;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        } else if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+        Stand stand = (Stand) obj;
+        if (this.m_ID.equals(stand.getID())) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
