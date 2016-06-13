81e2037e84529bbcb0683adea03fb4b66c36ac69
Guilherme Ferreira <Guilherme Ferreira>
2016-06-13 12:24:43 +0100

    [UC9][implementation] codigo completo + interface gráfica funcional

--- src/main/java/lapr/project/model/AbstractListModelRecursos.java
+++ src/main/java/lapr/project/model/AbstractListModelRecursos.java
@@ -10,7 +10,7 @@ import javax.swing.AbstractListModel;
 
 /**
  *
- * @author guima
+ * @author guilherme
  */
 public class AbstractListModelRecursos extends AbstractListModel {
 
@@ -20,11 +20,20 @@ public class AbstractListModelRecursos extends AbstractListModel {
         this.m_listaRecursos = listaRecursos;
     }
 
+    /**
+     *
+     * @return tamanho da lista de recursos (numero de recursos existentes)
+     */
     @Override
     public int getSize() {
         return m_listaRecursos.size();
     }
 
+    /**
+     * 
+     * @param i
+     * @return recurso na posição i, da lista de recursos
+     */
     @Override
     public Object getElementAt(int i) {
         return m_listaRecursos.get(i);
