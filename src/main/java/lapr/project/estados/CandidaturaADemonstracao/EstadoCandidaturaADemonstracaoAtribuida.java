864b5b4afdf634458247a918017151dcedc6ad65
Ricardo Jorge Afonso Catalão <1150385@isep.ipp.com>
2016-06-14 10:26:50 +0100

    Estados das candidaturas a demonstração modificadas para ficarem de acordo com o novo design [Implementation]

--- src/main/java/lapr/project/estados/CandidaturaADemonstracao/EstadoCandidaturaADemonstracaoAtribuida.java
+++ src/main/java/lapr/project/estados/CandidaturaADemonstracao/EstadoCandidaturaADemonstracaoAtribuida.java
@@ -6,59 +6,36 @@ import lapr.project.model.CandidaturaADemonstracao;
  *
  * @author Ana Leite
  */
-public class EstadoCandidaturaADemonstracaoAtribuida implements EstadoCandidaturaADemonstracao {
+public class EstadoCandidaturaADemonstracaoAtribuida extends EstadoCandidaturaADemonstracao {
 
     CandidaturaADemonstracao m_cand;
-    
-    public EstadoCandidaturaADemonstracaoAtribuida(CandidaturaADemonstracao c){
-        m_cand = c;
-    }
-    
-    @Override
-    public boolean setEstadoCandidaturaInstanciada() {
-        return false;
-    }
 
-    @Override
-    public boolean setEstadoCandidaturaCriada() {
-        return false;
+    public EstadoCandidaturaADemonstracaoAtribuida(CandidaturaADemonstracao c) {
+        m_cand = c;
     }
 
     @Override
     public boolean setEstadoCandidaturaAtribuida() {
-        return false;
+        return true;
     }
-    
-     @Override
+
+    @Override
     public boolean setEstadoCandidaturaAvaliada() {
-        if (valida()){
+        if (valida()) {
             m_cand.setEstado(new EstadoCandidaturaADemonstracaoAvaliada(m_cand));
-        return true;
-        } else return false;
-    }
-
-    @Override
-    public boolean isEstadoCandidaturaInstanciada() {
-        return false;
-    }
-
-    @Override
-    public boolean isEstadoCandidaturaCriada() {
-        return false;
-    }
-
-    @Override
-    public boolean isEstadoCandidaturaAtribuida() {
-        return false;
-    }
-
-    @Override
-    public boolean isEstadoCandidaturaAvaliada() {
-        return true;
+            return true;
+        } else {
+            return false;
+        }
     }
 
     private boolean valida() {
         //valida se tem as coisas todas necessárias para passar ao próximo estado (verificação de atributos)
+        return true;
+    }
+
+    @Override
+    public boolean isEstadoCandidaturaAtribuida() {
         return true;
     }
 }
