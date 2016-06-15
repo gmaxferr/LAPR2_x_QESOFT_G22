90a84058af5cebb568facbd3c1e671119cd1a4be
Cardoso <1150943@isep.ipp.pt>
2016-06-15 10:05:48 +0100

    Alterações na UI do UC1 [UC1] [Implementation]

--- src/main/java/lapr/project/ui/ModelComboBoxDias.java
+++ src/main/java/lapr/project/ui/ModelComboBoxDias.java
@@ -12,30 +12,81 @@ import javax.swing.ComboBoxModel;
  *
  * @author JoãoCardoso aka K4rd050
  */
-public class ModelComboBox28Dias extends AbstractListModel implements ComboBoxModel{
-    
-int[] dias = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28};
+public class ModelComboBoxDias extends AbstractListModel implements ComboBoxModel {
 
-    
+    int[] dias_28 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28};
+    int[] dias_29 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29};
+    int[] dias_30 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
+    int[] dias_31 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
+
     private Object selectedItem;
-    
+    private int opcao;
+
+    public ModelComboBoxDias(int opcao) {
+        this.opcao = opcao;
+    }
+
     @Override
     public int getSize() {
-        return dias.length;
+        switch (this.opcao) {
+            case 28:
+                return dias_28.length;
+            case 29:
+                return dias_29.length;
+            case 30:
+                return dias_30.length;
+            case 31:
+                return dias_31.length;
+            default:
+                return 31;
+        }
     }
 
     @Override
     public Object getElementAt(int index) {
-        return dias[index];
+        switch (this.opcao) {
+            case 28:
+                return dias_28[index];
+            case 29:
+                return dias_29[index];
+            case 30:
+                return dias_30[index];
+            case 31:
+                return dias_31[index];
+            default:
+                return null;
+        }
     }
 
     @Override
     public void setSelectedItem(Object anItem) {
-        selectedItem = (Integer) anItem;
+        switch (this.opcao) {
+            case 28:
+                selectedItem = (Integer) anItem;
+            case 29:
+                selectedItem = (Integer) anItem;
+            case 30:
+                selectedItem = (Integer) anItem;
+            case 31:
+                selectedItem = (Integer) anItem;
+            default:
+
+        }
     }
 
     @Override
     public Object getSelectedItem() {
-        return selectedItem;
+        switch (this.opcao) {
+            case 28:
+                return selectedItem;
+            case 29:
+                return selectedItem;
+            case 30:
+                return selectedItem;
+            case 31:
+                return selectedItem;
+            default:
+                return null;
+        }
     }
 }
