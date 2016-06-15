8c53a43a1f0066c589b21f2c94256ad80382d216
AnaLeite <1150368@isep.ipp.pt>
2016-06-15 11:31:57 +0100

    Testes controller confirmar registo utilizador concluídos

---
+++ src/test/java/lapr/project/controller/ConfirmarRegistoControllerTest.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.controller;
+
+import java.util.ArrayList;
+import lapr.project.model.CentroExposicoes;
+import lapr.project.model.Utilizador;
+import lapr.project.registos.RegistoUtilizadores;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Ana
+ */
+public class ConfirmarRegistoControllerTest {
+
+    private ConfirmarRegistoController instance;
+    private RegistoUtilizadores ru;
+    private ArrayList<Utilizador> listaUtilizadores;
+    private CentroExposicoes ce;
+
+    public ConfirmarRegistoControllerTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        ce = new CentroExposicoes();
+        ru = ce.getRegistoUtilizadores();
+        listaUtilizadores = ru.getListaNovosRegistos();
+        instance = new ConfirmarRegistoController(ce);
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getRegistoUtilizadores method, of class
+     * ConfirmarRegistoController.
+     */
+    @Test
+    public void testGetRegistoUtilizadores() {
+        System.out.println("getRegistoUtilizadores");
+        instance.getRegistoUtilizadores();
+    }
+
+    /**
+     * Test of getListaNovosRegistos method, of class
+     * ConfirmarRegistoController.
+     */
+    @Test
+    public void testGetListaNovosRegistos() {
+        System.out.println("getListaNovosRegistos");
+        instance.getRegistoUtilizadores();
+        instance.getListaNovosRegistos();
+        ArrayList<Utilizador> expResult = listaUtilizadores;
+        ArrayList<Utilizador> result = instance.getListaNovosRegistos();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setUtilizadorRegistado method, of class
+     * ConfirmarRegistoController.
+     */
+    @Test
+    public void testSetUtilizadorRegistado() {
+        System.out.println("setUtilizadorRegistado");
+        Utilizador u = new Utilizador();
+        instance.setUtilizadorRegistado(u);
+    }
+
+}
