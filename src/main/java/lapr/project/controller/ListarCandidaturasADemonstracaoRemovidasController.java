96b5fc4b56c0218756a3d3b2ffbe9d6671baec40
AnaLeite <1150368@isep.ipp.pt>
2016-06-18 17:39:17 +0100

    Correção do controller listar candidaturas a demonstração removidas

--- src/main/java/lapr/project/controller/ListarCandidaturasADemonstracaoRemovidasController.java
+++ src/main/java/lapr/project/controller/ListarCandidaturasADemonstracaoRemovidasController.java
@@ -1,17 +1,19 @@
-
 package lapr.project.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import lapr.project.model.CandidaturaADemonstracao;
 import lapr.project.model.CentroExposicoes;
 import lapr.project.model.Demonstracao;
 import lapr.project.model.Exposicao;
+import lapr.project.model.Organizador;
 import lapr.project.registos.RegistoCandidaturasADemonstracaoRemovidas;
 import lapr.project.registos.RegistoDemonstracoes;
 
 /**
- * Representação do Controller do caso de uso - listar candidaturas a demonstracao removidas
- * 
+ * Representação do Controller do caso de uso - listar candidaturas a
+ * demonstracao removidas
+ *
  * @author Ana Leite
  */
 public class ListarCandidaturasADemonstracaoRemovidasController {
@@ -20,54 +22,59 @@ public class ListarCandidaturasADemonstracaoRemovidasController {
      * Registo candidaturas a demonstracao removidas
      */
     private RegistoCandidaturasADemonstracaoRemovidas m_rcdr;
-    
+
     /**
      * Registo demonstrações
      */
     private RegistoDemonstracoes m_rd;
-    
+
     /**
      * Demonstração escolhida
      */
     private Demonstracao m_demonstracaoEscolhida;
-    
+
     /**
      * Exposição
      */
     private Exposicao m_Exposicao;
-    
+
     /**
      * Centro de Exposições
      */
     private CentroExposicoes m_centroExposicoes;
-    
+
     /**
-     * Constrói uma instância de ListarCandidaturasADemonstracacaoController recebendo como
-     * parâmetro o centro de exposições
+     * Constrói uma instância de ListarCandidaturasADemonstracacaoController
+     * recebendo como parâmetro o centro de exposições
      *
      * @param centroExposicoes centro de exposições
      */
-    public ListarCandidaturasADemonstracaoRemovidasController(CentroExposicoes centroExposicoes){
+    public ListarCandidaturasADemonstracaoRemovidasController(CentroExposicoes centroExposicoes) {
         this.m_centroExposicoes = centroExposicoes;
     }
-    
+
     /**
      * Guarda o registo de demonstrações
      */
-    public void getRegistoDemonstracoes(){
+    public void getRegistoDemonstracoes() {
         this.m_rd = m_Exposicao.getRegistoDemonstracoes();
     }
-    
+
     /**
      * Devolve a lista das demonstrações de um organizador
-     * 
+     *
      * @param usernameOrganizador username do organizador
      * @return lista das demonstrações de um organizador
      */
-    public List<Demonstracao> getListaDemonstracoesDoOrganizador(String usernameOrganizador){
-        return m_rd.getListaDemonstracoesDoOrganizador(usernameOrganizador);
+    public List<Demonstracao> getListaDemonstracoesDoOrganizadorEstadoConfirmada(String usernameOrganizador) {
+        for (Organizador o : m_Exposicao.getListaOrganizadores()) {
+            if (o.getUsernameOrganizador().equalsIgnoreCase(usernameOrganizador)) {
+                return m_rd.getListaDemonstracoesEstadoConfirmada();
+            }
+        }
+        return new ArrayList<>();
     }
-    
+
     /**
      * Modifica a demonstração.
      *
@@ -76,14 +83,14 @@ public class ListarCandidaturasADemonstracaoRemovidasController {
     public void setDemonstracao(Demonstracao d) {
         this.m_demonstracaoEscolhida = d;
     }
-    
+
     /**
      * Guarda o registo de candidaturas a demonstração removidas.
      */
     public void getRegistoCandidaturasADemonstracaoRemovidas() {
         this.m_rcdr = m_demonstracaoEscolhida.getRegistoCandidaturasADemonstracaoRemovidas();
     }
-    
+
     /**
      * Devolve a lista de candidaturas a demonstração removidas
      *
@@ -92,5 +99,5 @@ public class ListarCandidaturasADemonstracaoRemovidasController {
     public List<CandidaturaADemonstracao> getListaCandidaturasADemonstracaoRemovidas() {
         return this.m_rcdr.getListaCandidaturasADemonstracaoRemovidas();
     }
-    
+
 }
