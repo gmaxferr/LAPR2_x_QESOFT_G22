e5b28a45f0787c937a74129c850a68dad046491c
Ricardo Jorge Afonso Catalão <1150385@isep.ipp.com>
2016-06-22 12:14:54 +0100

    I/O XML criado para as classes AtribuicaoCandidaturaDemonstracao e RegistoAtribuicoesDemonstracao [Implementation] [UC31] [UC32]

--- src/main/java/lapr/project/model/AtribuicaoCandidaturaDemonstracao.java
+++ src/main/java/lapr/project/model/AtribuicaoCandidaturaDemonstracao.java
@@ -1,30 +1,41 @@
 package lapr.project.model;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.ParserConfigurationException;
+import static lapr.project.model.AtribuicaoCandidatura.ROOT_ELEMENT_NAME;
 import lapr.project.registos.RegistoCandidaturasADemonstracoes;
 import lapr.project.registos.RegistoFaeAvaliacao;
 import lapr.project.registos.RegistoUtilizadores;
+import lapr.project.utils.Exportable;
+import lapr.project.utils.Importable;
+import lapr.project.utils.XMLParser;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  *
  * @author Ana Leite
  */
-public class AtribuicaoCandidaturaDemonstracao {
+public class AtribuicaoCandidaturaDemonstracao implements Importable<AtribuicaoCandidaturaDemonstracao>, Exportable {
 
     public static final String ROOT_ELEMENT_NAME = "AtribuicaoCandidaturaDemosntracao";
-    
+
     private CandidaturaADemonstracao m_candidatura;
-    
+
     private RegistoFaeAvaliacao m_rFaeAvaliacao;
-    
-    public AtribuicaoCandidaturaDemonstracao(CandidaturaADemonstracao candidaturaADemonstracao){
+
+    public AtribuicaoCandidaturaDemonstracao(CandidaturaADemonstracao candidaturaADemonstracao) {
         this.m_candidatura = candidaturaADemonstracao;
         this.m_rFaeAvaliacao = new RegistoFaeAvaliacao();
     }
-    
-    public CandidaturaADemonstracao getCandidaturaAssociada(){
+
+    public CandidaturaADemonstracao getCandidaturaAssociada() {
         return this.m_candidatura;
     }
-    
+
     /**
      * Devolve o registo de FAEAvaliação
      *
@@ -48,14 +59,52 @@ public class AtribuicaoCandidaturaDemonstracao {
         this.m_rFaeAvaliacao.fix(m_registoUtilizadores);
     }
 
-
     /**
      * TESTE USE ONLY
+     *
      * @param m_rFaeAvaliacao the m_rFaeAvaliacao to set
      */
     public void setRegistoFaeAvaliacao(RegistoFaeAvaliacao m_rFaeAvaliacao) {
         this.m_rFaeAvaliacao = m_rFaeAvaliacao;
     }
+
+    @Override
+    public AtribuicaoCandidaturaDemonstracao importContentFromXMLNode(Node node) throws ParserConfigurationException {
+        Document document = XMLParser.createDocument(node, true);
+
+        Node n = document.getChildNodes().item(0);
+        if (n.getNodeType() == Node.ELEMENT_NODE) {
+            Element elem = (Element) n;
+
+            Expositor expositor = new Expositor(null);
+            expositor.importContentFromXMLNode(elem.getElementsByTagName(Expositor.ROOT_ELEMENT_NAME).item(0));
+
+            this.m_candidatura = new CandidaturaADemonstracao(null, null);
+            this.m_candidatura.importContentFromXMLNode(elem.getElementsByTagName(CandidaturaADemonstracao.ROOT_ELEMENT_NAME).item(0));
+            this.m_rFaeAvaliacao = new RegistoFaeAvaliacao();
+            this.m_rFaeAvaliacao.importContentFromXMLNode(elem.getElementsByTagName(RegistoFaeAvaliacao.ROOT_ELEMENT_NAME).item(0));
+        }
+        return this;
+    }
+
+    @Override
+    public Node exportContentToXMLNode() {
+        Node node = null;
+
+        try {
+            Document document = XMLParser.createDocument();
+
+            Element elemBase = document.createElement(ROOT_ELEMENT_NAME);
+            document.appendChild(elemBase);
+
+            elemBase.appendChild(document.importNode(this.m_candidatura.exportContentToXMLNode(), true));
+            elemBase.appendChild(document.importNode(this.m_rFaeAvaliacao.exportContentToXMLNode(), true));
+
+            node = elemBase;
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(AtribuicaoCandidatura.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return node;
+    }
 }
-
-
--- src/main/java/lapr/project/registos/RegistoAtribuicoesDemonstracao.java
+++ src/main/java/lapr/project/registos/RegistoAtribuicoesDemonstracao.java
@@ -2,14 +2,28 @@ package lapr.project.registos;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import lapr.project.model.AtribuicaoCandidatura;
 import lapr.project.model.AtribuicaoCandidaturaDemonstracao;
 import lapr.project.model.CandidaturaAExposicao;
+import static lapr.project.registos.RegistoAtribuicoes.ROOT_ELEMENT_NAME;
+import lapr.project.utils.Exportable;
+import lapr.project.utils.Importable;
+import lapr.project.utils.XMLParser;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  *
  * @author Ana Leite
  */
-public class RegistoAtribuicoesDemonstracao {
+public class RegistoAtribuicoesDemonstracao implements Importable<RegistoAtribuicoesDemonstracao>, Exportable {
 
     public static final String ROOT_ELEMENT_NAME = "registoAtribuicoes";
 
@@ -58,7 +72,7 @@ public class RegistoAtribuicoesDemonstracao {
         }
         return listaAtrib;
     }
-    
+
     /**
      * Devolve a lista de atribuicaoCandidatura
      *
@@ -81,8 +95,8 @@ public class RegistoAtribuicoesDemonstracao {
     public void setAvaliacao(CandidaturaAExposicao candidaturaAExposicao, boolean decisao) {
         candidaturaAExposicao.setDecisao(decisao);
     }
-    
-     /**
+
+    /**
      * Conserta o valor das referências das variáveis guardados pelos objetos
      * que este objeto agrega.
      *
@@ -94,5 +108,62 @@ public class RegistoAtribuicoesDemonstracao {
             atr.fix(m_rcd, m_registoUtilizadores);
         }
     }
-}
 
+    /**
+     *
+     * @param node
+     * @return
+     */
+    @Override
+    public RegistoAtribuicoesDemonstracao importContentFromXMLNode(Node node) throws ParserConfigurationException {
+        Document doc = XMLParser.createDocument(node, true);
+
+        Node n = doc.getChildNodes().item(0);
+
+        if (n.getNodeType() == Node.ELEMENT_NODE) {
+            Element elem = (Element) n;
+
+            this.m_listaAtribuicao.clear();
+
+            NodeList nList = elem.getElementsByTagName(AtribuicaoCandidatura.ROOT_ELEMENT_NAME);
+            for (int i = 0; i < nList.getLength(); i++) {
+                Node n2 = nList.item(i);
+                AtribuicaoCandidaturaDemonstracao atrCand = new AtribuicaoCandidaturaDemonstracao(null);
+                atrCand.importContentFromXMLNode(n2);
+                this.m_listaAtribuicao.add(atrCand);
+            }
+        }
+
+        return this;
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public Node exportContentToXMLNode() {
+        Node node = null;
+
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.newDocument();
+
+            Element elementKeyword = document.createElement(ROOT_ELEMENT_NAME);
+
+            for (AtribuicaoCandidaturaDemonstracao atrCand : this.m_listaAtribuicao) {
+                Node n = atrCand.exportContentToXMLNode();
+                elementKeyword.appendChild(document.importNode(n, true));
+            }
+
+            document.appendChild(elementKeyword);
+
+            node = elementKeyword;
+
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(RegistoAtribuicoes.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return node;
+    }
+}
