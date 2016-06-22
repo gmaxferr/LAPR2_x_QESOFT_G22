edc3a12cb3ce4c9cf361c15a95453d9237a1f15d
Cardoso <1150943@isep.ipp.pt>
2016-06-22 12:40:18 +0100

    Implementação da UI do UC26 [UC26] [Implementation]

--- src/main/java/lapr/project/controller/AlterarCandidaturaDemonstracaoController.java
+++ src/main/java/lapr/project/controller/AlterarCandidaturaDemonstracaoController.java
@@ -79,8 +79,7 @@ public class AlterarCandidaturaDemonstracaoController {
         List<CandidaturaADemonstracao> lstCands = new ArrayList<>();
         for (Demonstracao d : lstDemos) {
             RegistoCandidaturasADemonstracoes rcd = d.getRegistoCandidaturasADemonstracao();
-            rcd.getListaCandidaturasADemonstracaoRep(m_emailExpositor);
-            lstCands.addAll(rcd.getListaCandidaturasADemonstracaoRep(m_emailExpositor));
+            lstCands.addAll(rcd.getListaCandidaturasADemonstracaoRep(d,m_emailExpositor));
         }
         return lstCands;
     }
