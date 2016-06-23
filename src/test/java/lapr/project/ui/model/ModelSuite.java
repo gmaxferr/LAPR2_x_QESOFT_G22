006a996b7710c18b6c093183182413fc883624af
Guilherme Ferreira <Guilherme Ferreira>
2016-06-23 10:05:37 +0100

    testes aos model da UI

---
+++ src/test/java/lapr/project/ui/model/ModelSuite.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.ui.model;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author guima
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({lapr.project.ui.model.ComboBoxModelTipoDeConflitoDemonstracaoTest.class, lapr.project.ui.model.ComboBoxModelExposicoesTest.class, lapr.project.ui.model.ComboBoxModelMecanismosTest.class, lapr.project.ui.model.ComboBoxModelDemonstracoesTest.class, lapr.project.ui.model.ModeloListaStandsTest.class, lapr.project.ui.model.ModeloJTableUtilizadoresTest.class, lapr.project.ui.model.ComboBoxModelCandidaturasMostraExpositoresTest.class, lapr.project.ui.model.ModeloJTableDecidirDemonstracoesTest.class, lapr.project.ui.model.ComBoBoxModelAtribuicoesDemonstracaoMostraCandidaturaTest.class, lapr.project.ui.model.ComboBoxModelFaeTest.class, lapr.project.ui.model.ComboBoxModelTipoDeConflitoTest.class, lapr.project.ui.model.ModelComboBoxDiasTest.class, lapr.project.ui.model.ComboBoxModelUtilizadoresTest.class, lapr.project.ui.model.ModeloJListUtilizadoresTest.class, lapr.project.ui.model.AbstractListModelConflitosDeInteresseCandidaturaAExposicaoTest.class, lapr.project.ui.model.AbstractListModelRecursosTest.class, lapr.project.ui.model.AbstractListModelConflitosDeInteresseCandidaturaADemonstracaoTest.class, lapr.project.ui.model.ModeloListaProdutosTest.class, lapr.project.ui.model.ComboBoxModelCandidaturaADemonstracaoTest.class, lapr.project.ui.model.AbstractListModelDemonstracoesTest.class, lapr.project.ui.model.ComboBoxModelCandidaturaAExposicaoTest.class, lapr.project.ui.model.ComboBoxModelAtribuicoesMostraCandidaturaTest.class, lapr.project.ui.model.ModeloListaDemonstracoesTest.class, lapr.project.ui.model.ModeloListExposicaoTest.class})
+public class ModelSuite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+}
---
+++ src/test/java/lapr/project/ui/model/ModeloListExposicaoTest.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.ui.model;
+
+import java.util.List;
+import lapr.project.model.Exposicao;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author guima
+ */
+public class ModeloListExposicaoTest {
+    
+    public ModeloListExposicaoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of add method, of class ModeloListExposicao.
+     */
+    @Test
+    public void testAdd() {
+        System.out.println("add");
+        List<Exposicao> l = null;
+        ModeloListExposicao instance = new ModeloListExposicao();
+        instance.add(l);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getSize method, of class ModeloListExposicao.
+     */
+    @Test
+    public void testGetSize() {
+        System.out.println("getSize");
+        ModeloListExposicao instance = new ModeloListExposicao();
+        int expResult = 0;
+        int result = instance.getSize();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getElementAt method, of class ModeloListExposicao.
+     */
+    @Test
+    public void testGetElementAt() {
+        System.out.println("getElementAt");
+        int i = 0;
+        ModeloListExposicao instance = new ModeloListExposicao();
+        Object expResult = null;
+        Object result = instance.getElementAt(i);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
