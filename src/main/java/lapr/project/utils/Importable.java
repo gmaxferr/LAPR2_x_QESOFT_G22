93d010d4f1861eab50daeea016b0a79f6fd78933
Guilherme - 1150623 <1150623@isep.ipp.pt>
2016-12-15 11:19:28 +0000

    Ignorem os erros para já

--- src/main/java/lapr/project/utils/Importable.java
+++ src/main/java/lapr/project/utils/Importable.java
@@ -4,17 +4,18 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Node;
 
 /**
- * All domains classes should include this interface.
- * Created by Nuno Bettencourt [NMB] on 29/05/16.
+ * All domains classes should include this interface. Created by Nuno
+ * Bettencourt [NMB] on 29/05/16.
  */
 @FunctionalInterface
 public interface Importable<T extends Exportable> {
-	/**
-	 * Imports the object content from an XML format.
-	 *
+
+    /**
+     * Imports the object content from an XML format.
+     *
      * @param node Node de onde importar data
-	 * @return Structured String containing content.
+     * @return Structured String containing content.
      * @throws javax.xml.parsers.ParserConfigurationException
-	 */
-	T importContentFromXMLNode(Node node) throws ParserConfigurationException;
+     */
+    T importContentFromXMLNode(Node node) throws ParserConfigurationException;
 }
